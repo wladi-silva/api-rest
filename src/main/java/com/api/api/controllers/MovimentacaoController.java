ee3dc0c035560162f690f30360139e8ea1663f1b
Wladimir <wladimir.silva@ucsal.edu.br>
2022-08-18 14:14:34 -0300

    github

--- src/main/java/com/api/api/controllers/MovimentacaoController.java
+++ src/main/java/com/api/api/controllers/MovimentacaoController.java
@@ -1,5 +1,7 @@
 package com.api.api.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.api.models.Movimentacao;
 import com.api.api.repositories.MovimentacaoRepository;
 
+import antlr.collections.List;
+
 @RestController
 public class MovimentacaoController {
     
@@ -16,8 +20,8 @@ public class MovimentacaoController {
     private MovimentacaoRepository movimentacaoRepository;
 
     @PostMapping
-    public ResponseEntity<Movimentacao> getMovimentacao() {
-        return ResponseEntity.status(HttpStatus.OK).body(movimentacaoRepository.findAll());
+    public Optional<Movimentacao> novaMovimentacao() {
+        return ResponseEntity.status(HttpStatus.CREATED).body(movimentacaoRepository.findAll());
     }
 
     
