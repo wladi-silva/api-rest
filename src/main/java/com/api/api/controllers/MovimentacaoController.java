7a6093ab6654c2e52b96498c699e4933bea7b52d
Wladimir <wladimir.silva@ucsal.edu.br>
2022-08-30 15:22:03 -0300

    versão para teste em casa

--- src/main/java/com/api/api/controllers/MovimentacaoController.java
+++ src/main/java/com/api/api/controllers/MovimentacaoController.java
@@ -1,29 +1,46 @@
 package com.api.api.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.api.models.Movimentacao;
-import com.api.api.repositories.MovimentacaoRepository;
+import com.api.api.services.MovimentacaoService;
 
-import antlr.collections.List;
 
 @RestController
+@RequestMapping("movimentacao")
 public class MovimentacaoController {
     
     @Autowired
-    private MovimentacaoRepository movimentacaoRepository;
+    private MovimentacaoService movimentacaoService;
 
     @PostMapping
-    public Optional<Movimentacao> novaMovimentacao() {
-        return ResponseEntity.status(HttpStatus.CREATED).body(movimentacaoRepository.findAll());
+    public ResponseEntity<Object> saveMovimentacao(@RequestBody Movimentacao movimentacao) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(movimentacaoService.save(movimentacao));
     }
 
+    @GetMapping
+    public ResponseEntity<List<Movimentacao>> allMovimentacao() {
+        return ResponseEntity.status(HttpStatus.OK).body(movimentacaoService.findAll());
+    }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> oneMovimentacao(@PathVariable(value = "id") Long id) {
+        Optional<Movimentacao> movimentacaoOptional = movimentacaoService.findById(id);
+        if (!movimentacaoOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Não encontrado na base de dados.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(movimentacaoOptional.get());
+    }
 
 }
--- src/main/java/com/api/api/services/MovimentacaoService.java
+++ src/main/java/com/api/api/services/MovimentacaoService.java
@@ -1,13 +1,32 @@
 package com.api.api.services;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.api.api.models.Movimentacao;
 import com.api.api.repositories.MovimentacaoRepository;
 
 @Service
-public interface MovimentacaoService extends MovimentacaoRepository {
+public class MovimentacaoService {
     
+    @Autowired
+    public MovimentacaoRepository movimentacaoRepository;
 
+    @Transactional
+    public Movimentacao save(Movimentacao movimentacao) {
+        return movimentacaoRepository.save(movimentacao);
+    }
 
+    public List<Movimentacao> findAll() {
+        return movimentacaoRepository.findAll();
+    }
+
+    public Optional<Movimentacao> findById(Long id) {
+        return movimentacaoRepository.findById(id);
+    }
 
 }
